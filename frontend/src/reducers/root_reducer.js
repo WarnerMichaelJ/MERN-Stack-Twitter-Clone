e417acafdedbc956892fbe09ea24cbaed752a91c
Michael Warner <mwarner1040@gmail.com>
2019-07-07 18:41:21 -0400

    Preparing for a heroku push

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import errors from './errors_reducer';
 import session from './session_reducer';
+import tweets from './tweets_reducer';
 
 const RootReducer = combineReducers({
   errors,
-  session
+  session,
+  tweets
 });
 
 export default RootReducer;
